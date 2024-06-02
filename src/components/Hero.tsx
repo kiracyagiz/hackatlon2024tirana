60667c28762c5604b0fa9cc7a4a09d61fe17331c
kiracyagiz <105634620+kiracyagiz@users.noreply.github.com>
2024-06-02 09:29:52 +0200

    feat: redirect to main app

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,7 @@
 import { Button } from "./ui/button";
 import { HeroCards } from "./HeroCards";
 export const Hero = () => {
+
   return (
     <section className="container grid lg:grid-cols-2 place-items-center py-20 md:py-32 gap-10">
       <div className="text-center lg:text-start space-y-6">
@@ -25,7 +26,7 @@ export const Hero = () => {
         </p>
 
         <div className="space-y-4 md:space-y-0 md:space-x-4">
-          <Button className="w-full md:w-1/3">Get Started</Button>
+          <Button className="w-full md:w-1/3" onClick={() => window.open("https://priz-front.netlify.app/","_blank")}>Get Started</Button>
 
         </div>
       </div>
